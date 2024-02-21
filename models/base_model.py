9e286700f0f0716aaeef7690008892749580f1a6
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-02-21 21:58:06 +0200

    self.updated_at = datetime.now()

--- models/base_model.py
+++ models/base_model.py
@@ -36,6 +36,7 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
+        self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
 
