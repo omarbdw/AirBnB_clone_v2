be9c3d94a43bbd6fcf92f47a27fd8ba3a5a410dc
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-02-21 21:27:14 +0200

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,59 +1,57 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from models.base_model import Base
-import os
+from os import getenv
+
 
 class DBStorage:
-	__engine = None
-	__session = None
+    __engine = None
+    __session = None
 
-	def __init__(self):
-		user = os.getenv("HBNB_MYSQL_USER")
-		password = os.getenv("HBNB_MYSQL_PWD")
-		host = os.getenv("HBNB_MYSQL_HOST")
-		database = os.getenv("HBNB_MYSQL_DB")
-		env = os.getenv("HBNB_ENV")
+    def __init__(self):
+        env = getenv("HBNB_ENV")
 
-		self.__engine = create_engine(
-			'mysql+mysqldb://{}:{}@{}/{}'.format(
-				user, password, host, database),
-			pool_pre_ping=True)
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(
+                getenv('HBNB_MYSQL_USER'), getenv("HBNB_MYSQL_PWD"),
+                getenv("HBNB_MYSQL_HOST"), getenv("HBNB_MYSQL_DB")),
+            pool_pre_ping=True)
 
-		if env == 'test':
-			Base.metadata.drop_all(self.__engine)
+        if env == 'test':
+            Base.metadata.drop_all(self.__engine)
 
-		Base.metadata.create_all(self.__engine)
+        Base.metadata.create_all(self.__engine)
 
-		Session = scoped_session(sessionmaker(
-			bind=self.__engine, expire_on_commit=False))
-		self.__session = Session()
+        Session = scoped_session(sessionmaker(
+            bind=self.__engine, expire_on_commit=False))
+        self.__session = Session()
 
-	def all(self, cls=None):
-		from models import classes
-		objects = {}
-		if cls:
-			query = self.__session.query(classes[cls]).all()
-		else:
-			query = []
-			for cls in classes.values():
-				query += self.__session.query(cls).all()
-		for obj in query:
-			key = "{}.{}".format(type(obj).__name__, obj.id)
-			objects[key] = obj
-		return objects
+    def all(self, cls=None):
+        from models import classes
+        objects = {}
+        if cls:
+            query = self.__session.query(classes[cls]).all()
+        else:
+            query = []
+            for cls in classes.values():
+                query += self.__session.query(cls).all()
+        for obj in query:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            objects[key] = obj
+        return objects
 
-	def new(self, obj):
-		self.__session.add(obj)
+    def new(self, obj):
+        self.__session.add(obj)
 
-	def save(self):
-		self.__session.commit()
+    def save(self):
+        self.__session.commit()
 
-	def delete(self, obj=None):
-		if obj:
-			self.__session.delete(obj)
+    def delete(self, obj=None):
+        if obj:
+            self.__session.delete(obj)
 
-	def reload(self):
-		Base.metadata.create_all(self.__engine)
-		Session = scoped_session(sessionmaker(
-			bind=self.__engine, expire_on_commit=False))
-		self.__session = Session()
+    def reload(self):
+        Base.metadata.create_all(self.__engine)
+        Session = scoped_session(sessionmaker(
+            bind=self.__engine, expire_on_commit=False))
+        self.__session = Session()
