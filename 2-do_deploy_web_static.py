1073c7c8c36814eb7b2145f79c74b476c62de82e
omarbdw <omarbdw@yahoo.com>
2024-03-07 18:01:27 +0200

    1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,37 +1,30 @@
 #!/usr/bin/python3
-from fabric.api import env, put, run
-import os
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
+from fabric.api import put, run, env
+from os.path import exists
 env.hosts = ['100.25.45.246', '18.204.11.210']
 
 
 def do_deploy(archive_path):
-    if not os.path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        # Upload the archive to /tmp/ directory on the web server
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-
-        # Extract the archive to /data/web_static/releases/<archive
-        # filename without extension> on the web server
-        filename = os.path.basename(archive_path)
-        folder_name = '/data/web_static/releases/' + \
-                  os.path.splitext(filename)[0]
-        run('mkdir -p {}'.format(folder_name))
-        run('tar -xzf /tmp/{} -C {}'.format(filename, folder_name))
-
-        # Delete the archive from the web server
-        run('rm /tmp/{}'.format(filename))
-
-        # Delete the symbolic link /data/web_static/current
-        # from the web server
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-
-        # Create a new symbolic link /data/web_static/current
-        # on the web server
-        run('ln -s {} /data/web_static/current'.format(folder_name))
-
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except Exception:
+    except:
         return False
