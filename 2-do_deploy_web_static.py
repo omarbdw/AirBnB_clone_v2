38deed5ca46e8dfb4043765ac59d65ce527f4f4c
omarbdw <omarbdw@yahoo.com>
2024-03-07 17:50:20 +0200

    1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,46 +1,49 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to web servers
-"""
-from fabric.api import env, put, run
-from os.path import exists
+# Fabfile to distribute an archive to a web server.
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
-
-# Define the list of web servers
-env.hosts = ['100.25.45.246', '18.204.11.210']
+env.hosts = ["100.25.45.246", "18.204.11.210"]
 
 
 def do_deploy(archive_path):
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-    Distributes an archive to web servers
-    """
-    # Check if the archive file exists
-    if not exists(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
-    try:
-        # Extract the filename from the archive path
-        filename = archive_path.split('/')[-1]
-        # Define the path where the archive will be extracted
-        name = '/data/web_static/releases/' + \
-            "{}".format(filename.split('.')[0])
-        # Define the temporary path for the archive
-        tmpName = "/tmpName/" + filename
-        # Upload the archive to the temporary path on the server
-        put(archive_path, "/tmpName/")
-        # Create the directory for the extracted files
-        run("mkdir -p {}/".format(name))
-        # Extract the archive to the specified directory
-        run("tar -xzf {} -C {}/".format(tmpName, name))
-        # Remove the temporary archive file
-        run("rm {}".format(tmpName))
-        # Move the contents of the extracted directory to the parent directory
-        run("mv {}/web_static/* {}/".format(name, name))
-        # Remove the empty web_static directory
-        run("rm -rf {}/web_static".format(name))
-        # Remove the current symbolic link
-        run("rm -rf /data/web_static/current")
-        # Create a new symbolic link to the extracted directory
-        run("ln -s {}/ /data/web_static/current".format(name))
-        return True
-    except Exception:
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
