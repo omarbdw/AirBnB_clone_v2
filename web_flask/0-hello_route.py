9d77bdf3fe800a0371b1d9843ea4d0b53f41aa35
omarbdw <omarbdw@yahoo.com>
2024-03-21 14:13:08 +0200

    6

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+	"""
+	Returns a greeting message.
+
+	Returns:
+		str: The greeting message 'Hello HBNB!'.
+	"""
+	return 'Hello HBNB!'
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000)
