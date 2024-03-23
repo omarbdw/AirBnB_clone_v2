f61f76115df0cd322123e46a301d7072a87b9413
omarbdw <omarbdw@yahoo.com>
2024-03-23 17:31:44 +0200

    112254

--- models/place.py
+++ models/place.py
@@ -8,23 +8,11 @@ from models.review import Review
 import models
 import os
 
-
-place_amenity = Table(
-    'place_amenity',
-    Base.metadata,
-    Column(
-        'place_id',
-        String(60),
-        ForeignKey('places.id'),
-        primary_key=True,
-        nullable=False),
-    Column(
-        'amenity_id',
-        String(60),
-        ForeignKey('amenities.id'),
-        primary_key=True,
-        nullable=False)
-)
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel):
