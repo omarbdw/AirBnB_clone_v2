90ae4172f6d0a33fea179a9aac91a6c010e56bf8
omarbdw <omarbdw@yahoo.com>
2024-03-23 08:43:31 +0200

    8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,3 +1,23 @@
 #!/usr/bin/python3
+
+from flask import Flask, render_template
+from models import storage
+
 """ Module that starts a Flask web application """
 
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_session(exception):
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
