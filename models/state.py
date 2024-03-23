e78eff1584d5dc31ec12b94528b6881426a99129
omarbdw <omarbdw@yahoo.com>
2024-03-23 09:05:11 +0200

    city

--- models/state.py
+++ models/state.py
@@ -12,15 +12,15 @@ class State(BaseModel):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', cascade='all, delete', backref='state')
+        cities = relationship("City", backref="state")
     else:
         @property
         def cities(self):
-            """Getter attribute that returns the list of City instances with state_id equals to the current State.id"""
+            """Getter attribute that returns the list of City instances
+            with state_id equals to the current State.id"""
             city_list = []
-            for city in storage.all(City).values():
+            for city in models.storage.all(City).values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
