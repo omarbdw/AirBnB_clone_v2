a0a8fc483c4a8b03499a47fd590093d8467624f6
omarbdw <omarbdw@yahoo.com>
2024-03-24 09:23:55 +0200

    1

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class to manage the storage of the HBnB project"""
 
-import models
-from models.amenity import Amenity
-from models.base_model import BaseModel, Base
+from sqlalchemy import create_engine
+from os import getenv
+from models.base_model import Base
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
+from sqlalchemy.orm import sessionmaker, scoped_session
 from models.user import User
-from os import getenv
-import sqlalchemy
-from sqlalchemy import create_engine
+from models.amenity import Amenity
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 classes = {"Amenity": Amenity, "City": City,
@@ -40,14 +39,26 @@ class DBStorage:
 
     def all(self, cls=None):
         """query on the current database session"""
-        new_dict = {}
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                objs = self.__session.query(classes[clss]).all()
-                for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
-                    new_dict[key] = obj
-        return (new_dict)
+        result = {}
+        query_rows = []
+
+        if cls:
+            '''Query for all objects belonging to cls'''
+            if type(cls) is str:
+                cls = eval(cls)
+            query_rows = self.__session.query(cls)
+            for obj in query_rows:
+                key = '{}.{}'.format(type(obj).__name__, obj.id)
+                result[key] = obj
+            return result
+        else:
+            '''Query for all types of objects'''
+            for name, value in classes.items():
+                query_rows = self.__session.query(value)
+                for obj in query_rows:
+                    key = '{}.{}'.format(name, obj.id)
+                    result[key] = obj
+            return result
 
     def new(self, obj):
         """add the object to the current database session"""
