cef7cb6a9a8a9ad7bc5ba0815d0e83631128bc4d
omarbdw <omarbdw@yahoo.com>
2024-03-24 11:55:46 +0200

    w

--- models/city.py
+++ models/city.py
@@ -11,6 +11,7 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        id = Column(String(60), primary_key=True, nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
         places = relationship("Place", backref="cities")
